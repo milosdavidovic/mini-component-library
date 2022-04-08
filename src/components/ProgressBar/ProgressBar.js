1e188fb80f8fcd489974773688fd861b60fe98be
Milos Davidovic <m.davidovic@vegait.rs>
2022-04-08 19:22:09 +0200

    progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,36 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <ProgressBarWrapper
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+    >
+      <Bar>{value}</Bar>
+    </ProgressBarWrapper>
+  );
 };
+
+const ProgressBarWrapper = styled.div`
+  background-color: ${COLORS.grey500};
+  width: 200px;
+  height: 20px;
+  border: 1px solid ${COLORS.grey50};
+  border-radius: 8px;
+  padding: 2px 0;
+`;
+
+const Bar = styled.div`
+  background-color: ${COLORS.primary};
+  width: 50%;
+  height: 100%;
+  border-radius: 8px 8px 0 0;
+`;
 
 export default ProgressBar;
